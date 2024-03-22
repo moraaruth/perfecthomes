ae770a3186fa03e70d8c7b27ef3ea452a73aeeba
rmnyangau <rmnyangau@safaricom.co.ke>
2024-03-22 15:02:11 +0300

    adding link functionality and login

--- perfecthomes/app/properties/add/page.jsx
+++ perfecthomes/app/properties/add/page.jsx
@@ -1,10 +1,11 @@
+import Link from "next/link";
 const PropertAddPage
 
  = () => {
     return (
         <div>PropertAddPage
              <h1 className="text-3xl">Welcome</h1>
-        <Link href="/properties">Show Properties</Link>
+        <Link href="/properties/add">Show Properties</Link>
             
         </div>
     )
