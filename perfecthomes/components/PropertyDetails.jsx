9ea565bd887efaaefa8a03c0badb39d2de8d9523
Ruth Moraa Nyangau <rmnyangau@safaricom.co.ke>
2025-10-04 09:58:49 +0300

    changing global styles

--- perfecthomes/components/PropertyDetails.jsx
+++ perfecthomes/components/PropertyDetails.jsx
@@ -49,7 +49,7 @@ const PropertyDetails = ({ property }) => {
           </div> */}
           <div className='flex items-center justify-center mb-4 border-b border-gray-200 md:border-b-0 pb-4 md:pb-0'>
             <div className='text-gray-500 mr-2 font-bold'>Sale</div>
-            <div className='text-2xl font-bold text-blue-500'>
+            <div className='text-2xl font-bold' style={{ color: '#800080' }}>
               {property.rates.sale ? (
                 `ksh${property.rates.sale.toLocaleString()}`
               ) : (
@@ -74,7 +74,7 @@ const PropertyDetails = ({ property }) => {
 
       <div className='bg-white p-6 rounded-lg shadow-md mt-6'>
         <h3 className='text-lg font-bold mb-6'>Description & Details</h3>
-        <div className='flex justify-center gap-4 text-blue-500 mb-4 text-xl space-x-9'>
+        <div className='flex justify-center gap-4 mb-4 text-xl space-x-9' style={{ color: '#800080' }}>
           <p>
             <FaBed className='inline-block mr-2' /> {property.beds}{' '}
             <span className='hidden sm:inline'>Beds</span>
