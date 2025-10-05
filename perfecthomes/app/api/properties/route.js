636e7e8c876103d396b2416e42e109d552901d96
Ruth Moraa Nyangau <rmnyangau@safaricom.co.ke>
2025-10-05 06:35:14 +0300

    changing redirect

--- perfecthomes/app/api/properties/route.js
+++ perfecthomes/app/api/properties/route.js
@@ -124,9 +124,10 @@ export const POST = async (request) => {
     const newProperty = new Property(propertyData);
     await newProperty.save();
 
-    return Response.redirect(
-      `${process.env.NEXTAUTH_URL}/properties/${newProperty._id}`
-    );
+    //return Response.redirect(
+   //   `${process.env.NEXTAUTH_URL}/properties/${newProperty._id}`
+    //);
+return Response.redirect(`/properties/${newProperty._id}`);
 
     // return new Response(JSON.stringify({ message: 'Success' }), {
     //   status: 200,
