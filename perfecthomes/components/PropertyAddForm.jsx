7d526eae2b051acc17dba7d10a5341ca0309ec7b
Ruth Moraa Nyangau <rmnyangau@safaricom.co.ke>
2025-10-06 23:23:00 +0300

    changes

--- perfecthomes/components/PropertyAddForm.jsx
+++ perfecthomes/components/PropertyAddForm.jsx
@@ -600,24 +600,6 @@ const PropertyAddForm = () => {
           />
         </div>
 
-        {/* <div className='mb-4'>
-          <label
-            htmlFor='images'
-            className='block text-gray-700 font-bold mb-2'
-          >
-            Images (Select images)
-          </label>
-          <input
-            type='file'
-            id='images'
-            name='images'
-            className='border rounded w-full py-2 px-3'
-            accept='image/*'
-            multiple
-            onChange={handleImageChange}
-            required
-          />
-        </div> */}
 
         <div>
           <button
