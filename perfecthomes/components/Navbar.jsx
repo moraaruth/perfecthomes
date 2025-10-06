8db2b35f756fdca5f0bd37889ad82098167a041e
Ruth Moraa Nyangau <rmnyangau@safaricom.co.ke>
2025-10-07 00:00:22 +0300

    changes

--- perfecthomes/components/Navbar.jsx
+++ perfecthomes/components/Navbar.jsx
@@ -96,7 +96,7 @@ const Navbar = () => {
               />
 
               <span className="hidden md:block text-2xl font-bold ml-2" style={{ color: '#800080' }}>
-                Perfect Homessssssssssssssssssssss
+                Perfect Home
               </span>
             </Link>
             {/* <!-- Desktop Menu Hidden below md screens --> */}
