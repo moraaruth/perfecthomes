7a988559eadbc9715582709668f15a62e36d9282
Ruth Moraa Nyangau <rmnyangau@safaricom.co.ke>
2025-10-17 18:17:19 +0300

    changes

--- perfecthomes/app/api/bookmarks/check/route.js
+++ perfecthomes/app/api/bookmarks/check/route.js
@@ -1,6 +1,5 @@
 import connectDB from '@/config/database';
 import User from '@/models/User';
-import { getSessionUser } from '@/utils/getSessionUser';
 
 export const dynamic = 'force-dynamic';
 
@@ -8,21 +7,13 @@ export const POST = async (request) => {
   try {
     await connectDB();
 
-    const { propertyId } = await request.json();
-
-    const sessionUser = await getSessionUser();
-
-    if (!sessionUser || !sessionUser.userId) {
-      return new Response('User ID is required', { status: 401 });
-    }
-
-    const { userId } = sessionUser;
+    const { propertyId, userId } = await request.json(); // receive userId from client
 
     // Find user in database
     const user = await User.findOne({ _id: userId });
 
     // Check if property is bookmarked
-    let isBookmarked = user.bookmarks.includes(propertyId);
+    let isBookmarked = user ? user.bookmarks.includes(propertyId) : false;
 
     return new Response(JSON.stringify({ isBookmarked }), {
       status: 200,
