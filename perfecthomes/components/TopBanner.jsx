e46f662ca54df7eb88e7b5d6d3c435a1762f27f7
Ruth Moraa Nyangau <rmnyangau@safaricom.co.ke>
2025-10-18 08:28:29 +0300

    topbanner changes

--- perfecthomes/components/TopBanner.jsx
+++ perfecthomes/components/TopBanner.jsx
@@ -13,7 +13,7 @@ const TopBanner = () => {
           </div>
           <div className="flex items-center space-x-2">
             <FaMapMarkerAlt className="text-white" />
-            <span>Westland</span>
+            <span>Westlands</span>
           </div>
         </div>
 
@@ -21,12 +21,41 @@ const TopBanner = () => {
         <div className="flex items-center space-x-4">
           <span>Follow us on:</span>
           <div className="flex items-center space-x-3">
-            <FaFacebook className="text-white hover:text-blue-500 cursor-pointer" />
+            <a
+              href="https://www.facebook.com/share/14NMtbUgq4a/?mibextid=wwXIfr"
+              target="_blank"
+              rel="noopener noreferrer"
+            >
+              <FaFacebook className="text-white hover:text-blue-500 cursor-pointer" />
+            </a>
+
             <FaTiktok className="text-white hover:text-pink-500 cursor-pointer" />
-            <FaTwitter className="text-white hover:text-blue-400 cursor-pointer" />
-            <FaLinkedin className="text-white hover:text-blue-600 cursor-pointer" />
+            {/* <FaTwitter className="text-white hover:text-blue-400 cursor-pointer" />
+            <FaLinkedin className="text-white hover:text-blue-600 cursor-pointer" /> */}
             <FaYoutube className="text-white hover:text-red-500 cursor-pointer" />
-            <FaInstagram className="text-white hover:text-pink-400 cursor-pointer" />
+            <a
+              href="https://wa.me/message/3PCINZDAGWVIH1"
+              target="_blank"
+              rel="noopener noreferrer"
+            >
+              <FaWhatsapp className="text-green-500 hover:text-green-700 cursor-pointer" />
+            </a>
+
+            {/* <FaInstagram className="text-white hover:text-pink-400 cursor-pointer" /> */}
+            <a
+              href="https://www.instagram.com/kenya_perfect_homes?igsh=dG9wZWF3NnF1ajZj&utm_source=qr"
+              target="_blank"
+              rel="noopener noreferrer"
+            >
+              <FaInstagram className="text-white hover:text-pink-400 cursor-pointer" />
+            </a>
+            <a
+              href="https://www.tiktok.com/@perfecthomes_1?_t=ZM-90dzr7MlxZA&_r=1"
+              target="_blank"
+              rel="noopener noreferrer"
+            >
+              <FaTiktok className="text-black hover:text-gray-700 cursor-pointer" />
+            </a>
           </div>
         </div>
       </div>
