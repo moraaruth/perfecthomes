ef8f0670a8e63575ca5456dbce9f9240e65fa6bc
Ruth Moraa Nyangau <rmnyangau@safaricom.co.ke>
2025-10-22 11:39:04 +0300

    footer changes

--- perfecthomes/components/Footer.jsx
+++ perfecthomes/components/Footer.jsx
@@ -1,6 +1,6 @@
 import logo from '@/assets/images/logo.jpg';
 import Image from 'next/image';
-import { FaPhone, FaMapMarkerAlt, FaFacebook, FaTiktok, FaTwitter, FaLinkedin, FaYoutube, FaInstagram } from 'react-icons/fa'
+import { FaPhone, FaMapMarkerAlt, FaFacebook, FaTiktok, FaTwitter, FaLinkedin, FaYoutube, FaInstagram, FaWhatsapp } from 'react-icons/fa'
 
 const Footer = () => {
   return (
